c3efd528989a20785cdd15a195385f7026e10d0e
Jack Chan <chenjieke@zju.edu.cn>
2022-05-27 14:57:26 +0800

    added double list

---
+++ DataStructure/1_LinearList/DoubleList.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+using namespace std;
+
+typedef int ElemType;
+typedef struct DNode {
+    ElemType data;
+    struct DNode* prior;
+    struct DNode* next;
+} DNode, * DLinkedList;
+
+void printList(DLinkedList DL) {
+    DLinkedList tail = DL->next;
+    while (tail != nullptr) {
+        printf("%3d", tail->data);
+        tail = tail->next;
+    }
+    printf("\n");
+}
+
+/**
+ * 头插法建立双向链表
+ */
+DLinkedList createList1(DLinkedList& DL) {
+    DL = new DNode();
+    DL->prior = nullptr;
+    DL->next = nullptr;
+
+    DNode* newNode;
+    int elem;
+    cin >> elem;
+    while (elem != -1) {
+        newNode = new DNode();
+        newNode->data = elem;
+        newNode->prior = DL;
+        newNode->next = DL->next;
+        if (DL->next != nullptr) { // 插入第一个结点时，不需要进行
+            DL->next->prior = newNode;
+        }
+        DL->next = newNode;
+        cin >> elem;
+    }
+    return DL;
+}
+
+/**
+ * 尾插法建立双向链表
+ */
+DLinkedList createList2(DLinkedList& DL) {
+    DL = new DNode();
+    DL->next = nullptr;
+    DL->prior = nullptr;
+
+    DNode* newNode;
+    DNode* tail = DL;
+    int elem;
+    cin >> elem;
+    while (elem != -1) {
+        newNode = new DNode();
+        newNode->data = elem;
+        newNode->prior = tail;
+        tail->next = newNode;
+        tail = newNode;
+        cin >> elem;
+    }
+    tail->next = nullptr;
+    return DL;
+}
+
+/**
+ * 按序号查找
+ */
+DNode* findKth(DLinkedList DL, int pos) {
+    if (pos == 0) {
+        return DL;
+    }
+    if (pos < 0) {
+        return nullptr;
+    }
+    int i = 1;
+    DNode* p = DL->next;
+    while (i < pos && p != nullptr) {
+        p = p->next;
+        ++i;
+    }
+    return p;
+}
+
+bool insertElem(DLinkedList DL, int pos, ElemType elem) {
+    DNode* p = findKth(DL, pos - 1);
+    if (p == nullptr) {
+        return false;
+    }
+    DNode* newNode = new DNode();
+    newNode->data = elem;
+    newNode->next = p->next;
+    newNode->prior = p;
+    p->next->prior = newNode;
+    p->next = newNode;
+    return true;
+
+}
+
+bool deleteKth(DLinkedList DL, int pos) {
+    DNode* p = findKth(DL, pos - 1);
+    if (p == nullptr) {
+        return false;
+    }
+    DNode* trash = p->next;
+    trash->next->prior = p;
+    p->next = trash->next;
+    delete trash;
+    trash = nullptr; // 注意避免野指针
+    return true;
+}
+
+int main() {
+    DLinkedList DL;
+    // createList1(DL);
+    createList2(DL);
+    printList(DL);
+
+    int k = 2;
+    printf("%d\n", findKth(DL, k)->data);
+
+    if (insertElem(DL, 2, 99)) {
+        printList(DL);
+    } else {
+        printf("insert failed.\n");
+    }
+
+    deleteKth(DL, 4);
+    printList(DL);
+    return 0;
+}
--- DataStructure/1_LinearList/LinkedList.cpp
+++ DataStructure/1_LinearList/LinkedList.cpp
@@ -33,7 +33,7 @@ LinkedList createList1(LinkedList& L) {
 LinkedList createList2(LinkedList& L) {
     L = new LNode(); // 带头结点的链表
     L->next = nullptr;
-    
+
     LinkedList tail = L;
     LNode* newNode;
     int elem;
@@ -58,10 +58,90 @@ void printList(LinkedList L) {
     cout << endl;
 }
 
+/**
+ * 按序号查找结点值
+ **/
+LNode* findKth(LinkedList L, int pos) {
+    LNode* p = L->next;
+    if (pos == 0) {
+        return L;
+    } 
+    if (pos < 0) {
+        return nullptr;
+    }
+    int i = 1;
+    while (p != nullptr && i < pos) {
+        p = p->next;
+        i++;
+    }
+    return p;
+}
+
+/**
+ * 按值查找
+ **/
+LNode* locateElem(LinkedList L, ElemType elem) {
+    LNode* p = L->next;
+    while (p != nullptr && elem != p->data) {
+        p = p->next;
+    }
+    return p;
+}
+
+bool insertElem(LinkedList L, int pos, ElemType elem) {
+    LinkedList p = findKth(L, pos - 1); // 获取前一个结点的地址
+    if (p == nullptr) {
+        return false;
+    }
+    LNode* newNode = new LNode();
+    // 或者写为
+    // LNode* newNode = (LNode*)malloc(sizeof(LNode));
+    newNode->data = elem;
+    newNode->next = p->next;
+    p->next = newNode;
+    return true;
+}
+
+bool deleteKth(LinkedList L, int pos) {
+    LinkedList p = findKth(L, pos - 1); // 获取前一个结点的地址
+    if (p == nullptr) {
+        return false;
+    }
+    LNode* trash = p->next;
+    p->next = trash->next;
+    delete trash;
+    trash = nullptr; // 避免野指针
+    // 或者写为
+    // free(trash);
+    // trash = NULL;
+    return true;
+}
+
 int main() {
     LinkedList L; // 链表头
     // createList1(L);
     createList2(L);
     printList(L);
+    
+    int k = 3;
+    cout << findKth(L, k)->data << endl;
+
+    int number = 3;
+    LNode* search = locateElem(L, number);
+    if (search != nullptr) {
+        cout << "find " << search->data << " successfully" << endl;
+    } else {
+        cout << "failed to find " << number << endl;
+    }
+
+    insertElem(L, 5, 50);
+    printList(L);
+
+    deleteKth(L, 1);
+    printList(L);
+
+    deleteKth(L, 1);
+    printList(L);
+
     return 0;
 }
